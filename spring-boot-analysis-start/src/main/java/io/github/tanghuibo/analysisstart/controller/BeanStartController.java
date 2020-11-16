369f735e8dda04dc09e407f1f7e8c8cf28cf26eb
tang_huibo <1215893264@qq.com>
2020-11-17 00:04:02 +0800

    添加启动分析

--- spring-boot-analysis-start/src/main/java/io/github/tanghuibo/analysisstart/controller/BeanStartController.java
+++ spring-boot-analysis-start/src/main/java/io/github/tanghuibo/analysisstart/controller/BeanStartController.java
@@ -4,10 +4,8 @@ import io.github.tanghuibo.analysisstart.store.BeanStartStore;
 import io.github.tanghuibo.analysisstart.vo.BeanStartInfo;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Comparator;
 import java.util.List;
 
 /**
@@ -19,15 +17,8 @@ import java.util.List;
 public class BeanStartController {
 
     @GetMapping
-    public List<BeanStartInfo> get(@RequestParam("order") String order) {
-        List<BeanStartInfo> list = BeanStartStore.getAll();
-        if("startTime".equals(order)) {
-            list.sort(Comparator.comparingLong(BeanStartInfo::getStartTime));
-        } else if("endTime".equals(order)) {
-            list.sort(Comparator.comparingLong(BeanStartInfo::getEndTime));
-        } else if("costTime".equals(order)) {
-            list.sort(Comparator.comparingLong(BeanStartInfo::getCostTime).reversed());
-        }
-        return list;
+    public List<BeanStartInfo> get() {
+        return BeanStartStore.getAll();
+
     }
 }
